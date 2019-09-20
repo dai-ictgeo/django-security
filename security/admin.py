2587f848452105056af7cadaaefad3ba4a856e22
Trevor Olexy <trevor.olexy@gmail.com>
2019-09-20 09:25:29 -0400

    add pwd expiry to admin

--- security/admin.py
+++ security/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 from security.models import PasswordExpiry, CspReport
 
-admin.site.register(PasswordExpiry)
+@admin.register(PasswordExpiry)
+class adminExpiry(admin.ModelAdmin):
+    list_display = ('user_id','password_expiry_date',)
 admin.site.register(CspReport)
